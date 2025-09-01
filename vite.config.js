1644149cb4d9c569fa805bbc9d2cdf1378364868
vijayashekarc <vijayashekarc@gmail.com>
2025-09-01 11:18:01 +0530

    Changed port number

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+        port: 3000, // Change this to your desired port
+      },
   plugins: [react()],
   define: {
     'process.env': {}
